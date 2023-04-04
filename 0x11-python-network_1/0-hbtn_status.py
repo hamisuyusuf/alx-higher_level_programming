4ed80008899a62d0f0f0513bc06f43951f8f4c29
hamisuyusuf <hamisuyusuf180@gmail.com>
2023-04-04 08:59:37 -0700

    Hot fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
+
+
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
@@ -6,3 +11,4 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
